8688f7ea84fb3b9f6ea765b41775a394c7dc86f2
Allison Ause <allison.ause@gmail.com>
2023-01-09 11:11:54 -0800

    playing with objects

--- Examples/General.js
+++ Examples/General.js
@@ -8,9 +8,21 @@ const crystal = {
 
 function checkCrystal(crystal) {
   for (let key in crystal) {
+    console.log('key', key)
     console.log('crystal', crystal[key])
   }
   return
 }
 
-checkCrystal(crystal)
+function addToObject(arr, obj) {
+  const key = arr[0]
+  console.log('key', key)
+  newObj = { ...obj, [key]: arr[1] }
+  delete newObj.origin
+  newObj.name = 'Malachite'
+  console.log('newObj', newObj)
+  return newObj
+}
+
+// checkCrystal(crystal)
+addToObject(['color', 'Silver'], crystal)
