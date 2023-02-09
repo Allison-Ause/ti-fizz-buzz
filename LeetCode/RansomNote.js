eb771017c4fa94af930cca34b57842db537cf6d2
Allison Ause <allison.ause@gmail.com>
2023-02-09 15:29:39 -0800

    added new attempt at RansomNpte

--- LeetCode/RansomNote.js
+++ LeetCode/RansomNote.js
@@ -8,3 +8,48 @@
 
 // Input: ransomNote = "a", magazine = "b"
 // Output: false
+
+// OBJECT WITH TWO SEPARATE LOOPS
+// RUNTIME 100ms (52.80%)
+// MEMORY 43.8mb (85.31%)
+
+var canConstruct = function (ransomNote, magazine) {
+  let map = {}
+  for (let i = 0; i < magazine.length; i++) {
+    {
+      map[magazine[i]] = (map[magazine[i]] || 0) + 1
+    }
+    //   map = {...map, [magazine[i]]: map[magazine[i]]++ || 1} DID NOT INCREASE
+  }
+
+  for (let letter of ransomNote) {
+    if (!map[letter]) {
+      return false
+    } else {
+      map[letter]--
+    }
+  }
+  return true
+}
+
+// RESTRUCTURED SYNTAX WITH FOR/OF LOOP TO POPULATE MAP.
+// LESS PERFORMANT
+
+// RUNTIME 103ms (46.57%)
+// MEMORY 45.2mb (48.68%)
+
+var canConstruct = function (ransomNote, magazine) {
+  let map = {}
+  for (const letter of magazine) {
+    map.hasOwnProperty(letter) ? map[letter]++ : (map[letter] = 1)
+  }
+
+  for (let letter of ransomNote) {
+    if (!map[letter]) {
+      return false
+    } else {
+      map[letter]--
+    }
+  }
+  return true
+}
