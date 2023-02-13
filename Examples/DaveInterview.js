3f85fbeecb45d0c8baa6fafbf0d92fa7e8a3ef5b
Allison Ause <allison.ause@gmail.com>
2023-02-13 15:42:28 -0800

    added two solves for Daves interview question

--- Examples/DaveInterview.js
+++ Examples/DaveInterview.js
@@ -1,4 +1,36 @@
-// given an array, return a new array without manipulating the original that returns every single instance of a number in the order they occur.
+// given an array, return a new array without manipulating the original that
+// returns every single instance of a number in the order they occur.
 // IE:
-// inputArray = [1, 2, 2, 3, 5, 8, 3]
+
 // outputArray = [1, 2, 3, 5, 8]
+
+const inputArray = [1, 2, 2, 3, 5, 8, 3]
+
+function instance(array) {
+  const instanceArr = array.reduce((acc, curr) => {
+    // if acc does not already have that number, add to array
+    if (!acc.includes(curr)) {
+      acc.push(curr)
+    }
+    return acc
+  }, [])
+  console.log('return arr:', instanceArr)
+  return instanceArr
+}
+
+function howMany(array) {
+  const newArr = []
+  for (let i = 0; i < array.length; i++) {
+    if (!newArr.find((x) => x === array[i])) {
+      newArr.push(array[i])
+    }
+    // if (!newArr.includes(array[i])) {
+    //   newArr.push(array[i])
+    // }
+  }
+  console.log('newArr:', newArr)
+  return newArr
+}
+
+instance(inputArray)
+howMany(inputArray)
