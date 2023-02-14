886499d29fbf6035d80509724413d84ff375f895
Allison Ause <allison.ause@gmail.com>
2023-02-14 14:24:22 -0800

    changed to ternary and added string and number manipulation

--- Examples/TestDome.js
+++ Examples/TestDome.js
@@ -2,21 +2,14 @@
 // to sum until return has only one digit.Company
 
 function createCheckDigit(membershipId) {
-  // Write the code that goes here.
-  // check digit for those ids
-  // add all digits; if sum is number with more than 1 digit, iterate again
   const digits = membershipId.split('')
-  console.log('digits:', digits)
-  const sum = digits.reduce((acc, curr) => acc + curr)
-  const finalNum = sum.split('')
-  console.log('finalNum:', finalNum)
-  if (finalNum.length > 1) {
-    return createCheckDigit(sum)
-  } else {
-    console.log('firing else')
-    return finalNum
+  // const sum = digits.reduce((acc, curr) => acc + Number(curr))
+  let sum = 0
+  for (let num of digits) {
+    sum += parseInt(num)
   }
+  const finalNum = sum.toString().length
+  return finalNum.length > 1 ? createCheckDigit(sum) : finalNum
 }
 
-console.log(createCheckDigit('55555'))
-Test
+createCheckDigit('555789879855')
