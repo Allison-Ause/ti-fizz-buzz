378628085d87ad84f5f827f4679257c675073d74
Allison Ause <allison.ause@gmail.com>
2023-03-09 10:53:13 -0800

    notes

--- LeetCode/ContainsDuplicateII.js
+++ LeetCode/ContainsDuplicateII.js
@@ -16,6 +16,27 @@ var containsNearbyDuplicate = function (nums, k) {
   return false
 }
 
+// SET SOLUTION
+// RUNTIME 107ms (81.57%)
+// MEMORY 58.5mb (65.83%)
+
+// originally attempted a WHILE loop at the top instead of an additional If statement
+// but found that it irreparably wouldn't work with the set.add placement
+var containsNearbyDuplicate = function (nums, k) {
+  const set = new Set()
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) {
+      return true
+    }
+    set.add(nums[i])
+    set.delete(nums[i - k])
+    if (set.size <= k) {
+      set.delete(nums[i - k])
+    }
+  }
+  return false
+}
+
 // ATTEMPT AT BRUTE FORCE SLIDING WINDOW. NOT PASSING ALL TEST
 
 // var containsNearbyDuplicate = function(nums, k) {
